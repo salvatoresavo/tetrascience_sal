7cc32db18de9c7a3af08d0206c27ac883af37b67
salvatoresavo <salvatoresavo@users.noreply.github.com>
2014-12-27 20:43:28 -0500

    Update engineTC_test.py

--- engineTC_test.py
+++ engineTC_test.py
@@ -7,11 +7,11 @@ import json
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MAX31855.MAX31855 as MAX31855
 
-device_name = 'sth@gmail.com_Thermocouple'
-interval = 3
-dev_url = "http://192.168.1.10:10080/api/log/tcdata"
-device_type = 'TC'
-device_apikey = 'dsf9usadf['
+#device_name = 'sth@gmail.com_Thermocouple'
+interval = 2
+#dev_url = "http://192.168.1.10:10080/api/log/tcdata"
+#device_type = 'TC'
+#device_apikey = 'dsf9usadf['
 
 apiKey = 'JU9aF9Khovrqu6VIT4ZkReOyWvS1uzfrFQnejTGdcXosTPxr'
 feedID = '1295198993'
@@ -28,12 +28,13 @@ sensor = MAX31855.MAX31855(CLK,CS,DO)
 
 while 1:
         tmp = sensor.readTempC()
-
-        print tmp
-        line = s.readline()
-      	print line
-	      upload = { "version":"1.0.0","datastreams":[{"id":"Temperature","current_value":tmp}]}
+	tmp_ic = sensor.readTempInternal() 
+        print tmp, tmp_ic
+        #line = s.readline()
+      	#print line
+	upload = { "version":"1.0.0","datastreams":[{"id":"Temperature","current_value":tmp}]}
+	upload_ic = { "version":"1.0.0","datastreams":[{"id":"IC_temp","current_value":tmp_ic}]}
       	upload_json = json.dumps(upload)
       	headers = {'X-ApiKey': apiKey}
       	r = requests.put('https://api.xively.com/v2/feeds/'+feedID+'.json', data=upload_json, headers = headers)
-	      time.sleep(interval)
+	time.sleep(interval)
